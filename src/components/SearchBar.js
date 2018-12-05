cff0885cd3d39394169f116ffab1afb1213baf59
Sasheem <sts12@me.com>
2018-12-05 11:31:50 -0500

    added comment

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,18 +2,26 @@ import React from 'react';
 
 class SearchBar extends React.Component {
 
-    onInputChange(event) {
-        console.log(event.target.value);
+    state = { term: '' };
+
+    onFormSubmit = (e) => {
+        // prevents default form behavior of submitting itself when user presses Enter button
+        e.preventDefault();
+        console.log(this.state.term);
     }
 
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label>Image Search</label>
                         {/* leaving () means this is a callback function */}
-                        <input type="text" onChange={this.onInputChange} />
+                        <input 
+                            type="text" 
+                            value={this.state.term} 
+                            onChange={(e) => this.setState({ term: e.target.value })} 
+                        />
                     </div>
                 </form>
             </div>
