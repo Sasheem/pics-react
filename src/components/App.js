b5afe145e78ee34d9507133254342a6694941ea3
Sasheem <sts12@me.com>
2018-12-07 12:57:59 -0500

    created ImageList component

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import unsplash from '../api/unsplash'; 
 import SearchBar from './SearchBar';
-
+import ImageList from './ImageList';
 class App extends React.Component {
 
     state = { images: [] };
@@ -20,7 +20,7 @@ class App extends React.Component {
         return (
             <div className="ui container" style={{ marginTop: '20px' }}>
                 <SearchBar onUserSubmit={this.onSearchSubmit}/>
-                <p>Found: {this.state.images.length} images</p>
+                <ImageList images={this.state.images} />
             </div>
         );
     }
